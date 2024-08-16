4e0788636d71d3e1c219741bc446066c844a7242
Dilip Babu <106616107+619DILIP@users.noreply.github.com>
2024-08-16 15:31:03 -0400

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -21,7 +21,7 @@ public class SeleniumTest {
         System.setProperty("webdriver.chrome.driver", "driver/chromedriver");//linux_64
 
         // Get file
-        File file = new File("ButtonCounter.html");
+        File file = new File("src/main/ButtonCounter.html");
         String path = "file://" + file.getAbsolutePath();
 
         // Create a new ChromeDriver instance
